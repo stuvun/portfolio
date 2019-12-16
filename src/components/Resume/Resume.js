850177720f89358cba9fb0d77e06256e3862d591
Steven Kim <sskim14@gmail.com>
2019-12-15 20:58:22 -0500

    Changed classes in Resume component and updated resume.pdf/resume.png.

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Resume.css";
+import resume from "../../images/resume.png";
+import resumeLink from "../../images/resume.pdf";
 
-const Resume = () => {
-  return <div className="resume-container"></div>;
-};
+class Resume extends Component {
+  render() {
+    return (
+      <div className="resume-container">
+        <a className="pdf-link" href={resumeLink}>
+          View PDF
+        </a>
+        <div className="image-container">
+          <img src={resume} className="resume" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Resume;
