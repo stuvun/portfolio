74bde0a09d12f6438a33e0dcaebb2c6ad1e16db3
Steven Kim <sskim14@gmail.com>
2019-12-15 21:41:37 -0500

    Imported pdf icon image in Resume component. Adjusted positioning of 'View PDF' link.

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import "./Resume.css";
 import resume from "../../images/resume.png";
 import resumeLink from "../../images/resume.pdf";
+import PDF from "../../images/pdf.png";
 
 class Resume extends Component {
   render() {
     return (
       <div className="resume-container">
         <a className="pdf-link" href={resumeLink}>
-          View PDF
+          <img src={PDF} rel="pdf" className="pdf" />
+          <span className="view">View PDF</span>
         </a>
         <div className="image-container">
           <img src={resume} className="resume" />
