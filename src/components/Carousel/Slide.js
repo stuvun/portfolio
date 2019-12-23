375d750b242a59ea2bb5a35ff364aa954f3cd404
Steven <sskim14@gmail.com>
2019-12-23 00:38:21 -0500

    Added json data to Slide component.

--- src/components/Carousel/Slide.js
+++ src/components/Carousel/Slide.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectData from "../ProjectData/ProjectData.json";
 
 const Slide = ({ image, label }) => {
   const styles = {
@@ -8,8 +9,41 @@ const Slide = ({ image, label }) => {
     backgroundPosition: "center center"
   };
   return (
-    <div className="slide" style={styles}>
-      <span className="label">{label}</span>
+    <div>
+      <div>
+        {ProjectData.map((project, index) => {
+          return (
+            <div key={index}>
+              <div>
+                <h1>{project.name}</h1>
+                <h2>Technologies Used</h2>
+                <ul>
+                  {project.technologies.map((technology, i) => {
+                    return <li key={i}>{technology}</li>;
+                  })}
+                </ul>
+                <a
+                  href={project.deployment}
+                  rel="deployed app"
+                  target="__blank noreferrer"
+                >
+                  Deployed App
+                </a>
+                <a
+                  href={project.repository}
+                  rel="repository"
+                  target="__blank noreferrer"
+                >
+                  Respository
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="slide" style={styles}>
+        <span className="label">{label}</span>
+      </div>
     </div>
   );
 };
