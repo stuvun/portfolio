ba685a5cd9e485042343eb94dedd40a6fa2f8ef8
Steven <sskim14@gmail.com>
2019-12-22 20:16:06 -0500

    Set up ProjectDetails component

---
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ProjectDetails.css";
+import Slide from "../Carousel/Slide";
+
+const ProjectDetails = () => {
+  return (
+    <div className="project-details">
+      <Slide></Slide>
+    </div>
+  );
+};
+
+export default ProjectDetails;
