c87ca0ef8ce3da82a9e29809287b632c7d96a456
Steven <sskim14@gmail.com>
2020-01-17 00:13:44 -0500

    replaced imports with require

--- src/components/Home/SectionTwo.js
+++ src/components/Home/SectionTwo.js
@@ -3,42 +3,8 @@ import ProjectData from "../../Data/ProjectData.json";
 import SkillData from "../../Data/SkillData.json";
 import Static from "./Static";
 import { ReactComponent as Divider } from "../../images/assets/divider.svg";
-import {
-  Calendar,
-  Simon,
-  Giphy,
-  Mock,
-  JavaScript,
-  Html,
-  Css,
-  ReactJS,
-  Python,
-  Node,
-  Postgres,
-  Mongodb,
-  Bootstrap,
-  Agile,
-  Illustrator,
-  Indesign
-} from "../../images/index";
 
 const SectionTwo = () => {
-  const images = [Calendar, Simon, Giphy, Mock];
-  const skills = [
-    JavaScript,
-    Html,
-    Css,
-    ReactJS,
-    Python,
-    Node,
-    Postgres,
-    Mongodb,
-    Bootstrap,
-    Agile,
-    Illustrator,
-    Indesign
-  ];
-  console.log(JSON.stringify(SkillData[1].image));
   return (
     <section id="section-two">
       <div className="navbar-container">
@@ -85,14 +51,14 @@ const SectionTwo = () => {
       </div>
       <h1 className="section-two-header">PROJECTS</h1>
       <div className="static-wrapper">
-        {images.map((image, i) => (
+        {ProjectData.map((project, i) => (
           <Static
             key={i}
-            image={image}
-            name={ProjectData[i].name}
-            deployment={ProjectData[i].deployment}
-            repository={ProjectData[i].repository}
-            technologies={ProjectData[i].technologies}
+            image={require(`../../images/gifs/${project.title}.gif`)}
+            name={project.name}
+            deployment={project.deployment}
+            repository={project.repository}
+            technologies={project.technologies}
           />
         ))}
       </div>
